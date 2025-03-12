f837394dc814db93e94349e89a0bf9fbb154ca76
mostafa0x <dodomido12350@gmail.com>
2025-03-12 08:46:08 -0700

    checkdb v5

--- Middlewares/CheckDBConnection.js
+++ Middlewares/CheckDBConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
- function CheckDB(req, res, next) {
-  if (mongoose.connection.readyState !== 1) {
+function CheckDB(req, res, next) {
+  if (mongoose.connection.readyState === (0 || 4)) {
     return res.status(500).json({
       message: "Server is not connected. Please try again later.",
     });
