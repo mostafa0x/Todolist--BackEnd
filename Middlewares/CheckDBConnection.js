653cbc9aff1e3740e48283b36e6cfaf21c7476e4
mostafa0x <dodomido12350@gmail.com>
2025-03-12 03:35:08 -0700

    xz

--- Middlewares/CheckDBConnection.js
+++ Middlewares/CheckDBConnection.js
@@ -2,13 +2,11 @@ const monges = require("mongoose");
 
 async function CheckDB(req, res, next) {
   if (monges.connection.readyState !== 1) {
-    return res
-      .status(500)
-      .json({
-        message:
-          "Server is not connected. Please try again later." +
-          monges.MongooseError.Messages,
-      });
+    return res.status(500).json({
+      message:
+        "Server is not connected. Please try again later." +
+        monges.Error.Messages,
+    });
   }
   next();
 }
