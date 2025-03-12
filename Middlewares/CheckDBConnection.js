c40247633e1a3bf5b8924b8b5377379afcb70a86
mostafa0x <dodomido12350@gmail.com>
2025-03-12 08:34:07 -0700

    checkdb edit.2

--- Middlewares/CheckDBConnection.js
+++ Middlewares/CheckDBConnection.js
@@ -1,7 +1,10 @@
 const monges = require("mongoose");
+const { DbTask } = require("../Model/Models");
 
 async function CheckDB(req, res, next) {
-  if (monges.connection.readyState !== 1) {
+  DbStats = await monges.connection.readyState;
+  
+  if (DbTask !== 1) {
     return res.status(500).json({
       message: "Server is not connected. Please try again later.",
     });
