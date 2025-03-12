e46a1fc1b2554d1032da88621dad811c75367657
mostafa0x <dodomido12350@gmail.com>
2025-03-12 03:54:02 -0700

    xzxz

--- Router/AuthRouter.js
+++ Router/AuthRouter.js
@@ -2,7 +2,7 @@ const AuthRouter = require("express").Router();
 const LoginConteroller = require("../Controller/LoginConteroller");
 const CheckDBMiddleware = require("../Middlewares/CheckDBConnection");
 
-AuthRouter.post("/login", CheckDBMiddleware, LoginConteroller.Login);
+AuthRouter.post("/login", LoginConteroller.Login);
 
 AuthRouter.post("/signin", CheckDBMiddleware, LoginConteroller.signin);
 
